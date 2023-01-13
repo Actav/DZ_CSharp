4271592379fe97b9c3769630c860af945fbd566b
Actav <actav@mail.com>
2023-01-14 00:52:40 +0300

    DZ ls_01

---
+++ 04/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход 
+принимает число (N), а на выходе показывает 
+все чётные числа от 1 до N.
+*/
+
+Console.WriteLine("Проерка является ли введенное число четным");
+Console.Write("Введите число: ");
+int length = Convert.ToInt32( Console.ReadLine() );
+
+for (int i = 2; i <= length; Console.Write(i + (i != length ? ", " : "\n")), i += 2);
