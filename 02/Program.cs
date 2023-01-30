b0099117a4ca1eb31ec8c2701363d51c54936faf
Actav <actav@mail.com>
2023-01-30 23:59:31 +0300

    DZ ls_05

---
+++ 02/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] numsArr = generateNumsArr(6, -99, 100);
+System.Console.WriteLine($"{arrToStr(numsArr)} -> {sumOddNum(numsArr)}");
+
+// -- FUNCs -- //
+int[] generateNumsArr(int n, int min, int max) {
+    int[] arrNum = new int[n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arrNum.Length; arrNum[i++] = rnd.Next(min, max + 1)) { }
+
+    return arrNum;
+}
+
+int sumOddNum(int[] numsArr) {
+    int sum = 0;
+    
+    for (int i = 1; i < numsArr.Length; i += 2) {
+        sum += numsArr[i];
+    }   
+
+    return sum;
+}
+
+string arrToStr(int[] nums){
+    return $"[{String.Join(", ", nums)}]";
+}
---
+++ 03/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+float[] floatNumsArr = generateFloatNumsArr(6, 0, 100);
+System.Console.WriteLine($"{arrToStr(floatNumsArr)} -> {sumMinMaxFloatNums(floatNumsArr)}");
+
+// -- FUNCs -- //
+float[] generateFloatNumsArr(int n, float min, float max) {
+    Random rnd = new Random();
+    float[] arrNum = new float[n];
+
+    for (int i = 0; i < arrNum.Length; i++) {
+        float randNum = rnd.NextSingle() * (max - min) + min;
+        arrNum[i] = (float)Math.Round(randNum, 3);
+    }
+
+    return arrNum;
+}
+
+float sumMinMaxFloatNums(float[] floatNumsArr) {
+    float min = floatNumsArr[0];
+    float max = floatNumsArr[0];
+
+    for (int i = 1; i < floatNumsArr.Length; i++) {
+        if (min > floatNumsArr[i]) {
+            min = floatNumsArr[i];
+        } else if (max < floatNumsArr[i]) {
+            max = floatNumsArr[i];
+        }   
+    }
+
+    return max - min;
+}
+
+string arrToStr(float[] nums){
+    return $"[{String.Join(", ", nums)}]";
+}
