2a64cc21a4e3e8c1b2c97bcee77a535dda6b0c0f
Actav <actav@mail.com>
2023-02-01 00:49:40 +0300

    DZ ls_06

---
+++ 01/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write("ВВедите М чисел через пробел: ");
+string input = Console.ReadLine() ?? "";
+int count = CountPositive(input);
+Console.WriteLine($"Введено положительных чисел: {count}");
+
+
+// --- FUNCs --- //
+int CountPositive(string input)
+{
+    string[] numbers = input.Split(' ');
+    int count = 0;
+    foreach (string number in numbers)
+    {
+        if (double.TryParse(number, out double value) && value > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ 02/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double b1 = getDoubleVal("b1");
+double k1 = getDoubleVal("k1");
+double b2 = getDoubleVal("b2");
+double k2 = getDoubleVal("k2");
+
+double x = GetIntersectionX(b1, k1, b2, k2);
+if (double.IsNaN(x))
+{
+    return;
+}
+double y = GetIntersectionY(x, k1, b1);
+
+Console.WriteLine("Точка пересечения: (" + x + ", " + y + ")");
+
+
+// ---- FUNCs --- //
+double getDoubleVal(string name){
+    double value;
+    Console.Write($"Введите значение для {name}: ");
+    while (!double.TryParse(Console.ReadLine(), out value))
+    {
+        Console.WriteLine($"Ошибочный ввод. Введите значение для {name}: ");
+    }
+
+    return value;
+}
+
+double GetIntersectionX(double b1, double k1, double b2, double k2) 
+{
+    if (k1 == k2)
+    {
+        Console.WriteLine("Прямые пралелны.");
+        return double.NaN;
+    }
+
+    return (b2 - b1) / (k1 - k2);
+}
+
+double GetIntersectionY(double x, double k1, double b1)
+{
+    return k1 * x + b1;
+}
